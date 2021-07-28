4ce1004b9d86b372b82eddd687d57033316b9c5c
owenttttt01 <82928400+owenttttt01@users.noreply.github.com>
2021-07-28 21:21:41 +0800

    upload script to run mysql_secure_installation

---
+++ scripts/mysql_secure.sh
@@ -0,0 +1,23 @@
+
+SECURE_MYSQL=$(expect -c "
+set timeout 10
+spawn mysql_secure_installation
+expect \"Enter current password for root (enter for none):\"
+send \"\r\"
+expect \"Change the root password?\"
+send \"y\r\"
+expect \"New password:\"
+send -- \"sfp@ssw0rD$ru87$\r\"
+expect \"Re-enter new password:\"
+send -- \"sfp@ssw0rD$ru87$\r\"
+expect \"Remove anonymous users?\"
+send \"y\r\"
+expect \"Disallow root login remotely?\"
+send \"y\r\"
+expect \"Remove test database and access to it?\"
+send \"y\r\"
+expect \"Reload privilege tables now?\"
+send \"y\r\"
+expect eof
+")
+
