970a441421b5b3770b2baa96542caa57a46f7ae3
owenttttt01 <82928400+owenttttt01@users.noreply.github.com>
2021-07-29 15:38:05 +0800

    Update db.sh

--- scripts/db.sh
+++ scripts/db.sh
@@ -1,13 +1,14 @@
 clear
+echo "Installing required packages."
 apt-get -y install python3-pip >/dev/null
 clear
-echo "Installation Status: 25% Complete"
+echo "Installation Status: 25% Complete."
 pip3 install paho-mqtt >/dev/null
 clear
-echo "Installation Status: 50% Complete"
+echo "Installation Status: 50% Complete."
 apt-get -y install curl >/dev/null
 clear
-echo "Installation Status: 75% Complete"
+echo "Installation Status: 75% Complete."
 apt-get -y install expect >/dev/null
 clear
 echo "Installation Status: 100% Complete. Starting Web Server now."
