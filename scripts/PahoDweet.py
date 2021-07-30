46b86d2a9d241afa8e73be560fda525ff2ccfedf
JustinXWT <73164010+JustinXWT@users.noreply.github.com>
2021-07-30 15:26:56 +0800

    Update PahoDweet.py

--- scripts/PahoDweet.py
+++ scripts/PahoDweet.py
@@ -1,14 +1,4 @@
 #!/usr/bin/env python3
-
-# Import the GPIO modules to control the GPIO pins of the Raspberry Pi
-# Uncomment the following only when testing on a physcial Rasberry Pi
-# Comment the following when testing on a Raspbian VM
-#import RPi.GPIO as GPIO
-
-# Import the Mock GPIO modules to control the Mock GPIO pins of the Raspberry Pi
-# Uncomment the following when testing on a Raspbian VM
-# Comment the following when testing on a physcial Rasberry Pi
-import EmulateGPIO as GPIO
 import ssl
 import dweepy
 import os
@@ -28,116 +18,6 @@ counter = 0
 old_dweet = dweepy.get_latest_dweet_for(myThing)
 old_created = old_dweet[0]['created']
 print(old_created)
-
-#Dweet Password
-master_password= "poggers"
-
-#Emulate Raspberry Pi
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-
-#Lights
-GreenLEDPin   = 20
-RedLEDPin   = 21
-BlueLEDPin   = 22
-YellowLEDPin   = 23
-
-GPIO.setup(GreenLEDPin, GPIO.OUT)
-GPIO.setup(RedLEDPin, GPIO.OUT)
-GPIO.setup(BlueLEDPin, GPIO.OUT)
-GPIO.setup(YellowLEDPin, GPIO.OUT)
-
-#Text Colour
-R  = '\033[31m' # red
-G  = '\033[32m' # green
-B  = '\033[34m' # blue
-W  = '\033[0m'  # white (normal)
-
-sensor_array = ['Daylight','Moisture','Thermometer','Motion']
-
-sensor_dict = {
-        "Smart Light":"OFF",
-        "Smart Sprinkler":"OFF",
-        "Smart Shelter":"OFF",
-        "Smart Scarecrow":"OFF",
-        }
-
-deviceStatus = "OFF"
-
-def sensor_check(sensor_name, measuring_value):
-  if sensor_name in sensor_array:
-      position = sensor_array.index(sensor_name)
-      #print(position)
-      if  (position == 0):
-          device_name = "SmartLight"
-          measuring_factor = "Light_Intensity"
-          device_id = "1"
-          if (measuring_value > 70):
-              sensor_dict["Smart Light"] = "ON"
-              deviceStatus = "ON"
-              GPIO.output(YellowLEDPin, True)
-          else:
-              sensor_dict["Smart Light"] = "OFF"
-              deviceStatus = "OFF"
-              GPIO.output(YellowLEDPin, False)
-
-      elif (position == 1):
-          device_name = "SmartSprinkler"
-          measuring_factor = "Soil_Moisture"
-          device_id = "2"
-          if (measuring_value < 30):
-              senor_dict["Smart Sprinkler"] = "ON"
-              deviceStatus = "ON"
-              GPIO.output(BlueLEDPin, True)
-          else:
-              senor_dict["Smart Sprinkler"] = "OFF"
-              deviceStatus = "OFF"
-              GPIO.output(BlueLEDPin, False)
-
-      elif (position == 2):
-          device_name = "SmartShelter"
-          measuring_factor = "Temperature"
-          device_id = "3"
-          if (measuring_value > 35):
-              sensor_dict["Smart Shelter"] = "ON"
-              deviceStatus = "ON"
-              GPIO.output(RedLEDPin, True)
-          else:
-              sensor_dict["Smart Shelter"] = "OFF"
-              deviceStatus = "OFF"
-              GPIO.output(RedLEDPin, False)
-
-      elif (position == 3):
-          device_name = "SmartScarecrow"
-          measuring_factor = "Motion_Detection"
-          device_id = "4"
-          if (measuring_value > 35):
-              sensor_dict["Smart Scarecrow"] = "ON"
-              deviceStatus = "ON"
-              GPIO.output(RedLEDPin, True)
-          else:
-              sensor_dict["Smart Scarecrow"] = "OFF"
-              deviceStatus = "OFF"
-              GPIO.output(RedLEDPin, False)
-
-      print('\nDevice Status = '+deviceStatus)
-      print('\nDevice Name = '+device_name)
-      print('\nMeasuring factor = '+measuring_factor)
-  else:
-      print("Sensor name not recognised")
-
-
-print("\nAll Smart Devices are currently OFF\n")
-GPIO.output(GreenLEDPin, False)
-GPIO.output(RedLEDPin, False)
-GPIO.output(YellowLEDPin, False)
-GPIO.output(BlueLEDPin, False)
-
-#print(GPIO.gpio_function(GreenLEDPin))
-#print(GPIO.gpio_function(RedLEDPin))
-#print(GPIO.gpio_function(YellowLEDPin))
-#print(GPIO.gpio_function(BlueLEDPin))
-
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
@@ -161,41 +41,39 @@ while True:
     #mqttc=mqtt.Client()
     latest_dweet = dweepy.get_latest_dweet_for(myThing)
     new_created = latest_dweet[0]['created']
-    latest_cont = latest_dweet[0]['content']
-    sorted_content = sorted(latest_cont.items(), key = lambda kv:kv[0])
-    password_tuple = sorted_content[0]
-    ap, dweet_password = password_tuple
-    if new_created != old_created and master_password == dweet_password:
+    if new_created != old_created:
         counter += 1
         print(str(counter) + " New Dweet Detected", end = "\n")
         old_created = new_created
-        #print(type(latest_dweet))
         print("Latest dweet: \n" + str(latest_dweet))
        
         latest_created = latest_dweet[0]["created"]
         print("\nTime of created dweet: " + str(latest_created))
     
-        junk, sensor = sorted_content[1]
-        junk2, zvalue = sorted_content[2]
-        print("\nContent of latest dweet: " + "sensor: " + str(sensor) + ", zvalue: " + str(zvalue))
-        
-        sensor_check(sensor, zvalue)
-        print("\nCurrent LED Status")
-        print("===========================================")
-        print("Smart Light (Yellow LED): " + sensor_dict["Smart Light"])
-        print("Smart Sprinkler" + B+"(Blue LED): " + W+ sensor_dict["Smart Sprinkler"])
-        print("Smart Shelter " + G+"(Green LED): " + W+ sensor_dict["Smart Shelter"])
-        print("Smart Scarecrow "+ R+"(Red LED): " + W+ sensor_dict["Smart Scarecrow"])
-        print("===========================================")
+        latest_content = latest_dweet[0]["content"]
+        print("\nContent of latest dweet: " + str(latest_content))
 
         str_created = json.dumps(latest_created)
+        str_content = json.dumps(latest_content)
 
         str_created_r = str_created.replace('"',"")
 
-        client.publish("farm", str_created_r + ",sensor:" + str(sensor) + ",zvalue:" + str(zvalue))
-        print("\nInformation Sent Across:\n", str_created_r + ",sensor:" + str(sensor) + ",zvalue:" + str(zvalue))
+        replace = '" {}'
+        for i in replace:
+            str_content = str_content.replace(i,"")
+        #str_content_r = str_content.replace(":",",")
+        #str_content_test = str_content.split(",")
+
+        #file_object = open('output.txt', 'a')
+        #file_object.write(str_created_r + ",")
+        #file_object.write(str_content + "\n")
+        #file_object.close()
+        client.publish("farm", str_created_r + "," +str_content)
         time.sleep(1)
         
-    else:
-        time.sleep(1)
+        #paho_result.wait_for_publish()          
+        #print(paho_result.wait_for_publish()) 
+        
+        
+
     time.sleep(10)
