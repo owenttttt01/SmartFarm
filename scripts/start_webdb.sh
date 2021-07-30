53db8d9daed485533702e8cedee734a13c6c934a
owenttttt01 <82928400+owenttttt01@users.noreply.github.com>
2021-07-30 19:00:12 +0800

    Update start_webdb.sh

--- scripts/start_webdb.sh
+++ scripts/start_webdb.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
 cd /root/iot_vol/SmartFarm/scripts
+chmod 700 start_webdb.sh
+chmod 700 db.sh
+chmod 700 docker_app.py
+chmod 700 mqtt_app.py
+chmod 700 init_mysql.sql
+chmod 700 mysql_secure.sh
 ./db.sh 2>/dev/null
-./mysql_secure.sh >/dev/null
-mysql < init_mysql.sql
+if [ $(/etc/init.d/mysql status | grep -o 'stopped' | wc -w) -eq 1 ]
+then
+	./mysql_secure.sh >/dev/null
+	mysql < init_mysql.sql
+fi
 python3 docker_app.py &
 python3 mqtt_app.py
