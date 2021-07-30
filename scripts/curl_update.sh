e64e3904511e384f5fc5454bd5b3bb26eab1993f
JoshuaKhooWX <73164041+JoshuaKhooWX@users.noreply.github.com>
2021-07-30 19:30:51 +0800

    Delete curl_update.sh

--- scripts/curl_update.sh
+++
@@ -1,4 +0,0 @@
-curl http://172.19.0.13:8080/update/1?Device=TEST_UPDATE_NAME
-curl http://172.19.0.13:8080/update/2?DeviceTime=TEST_UPDATE_TIME
-curl http://172.19.0.13:8080/update/3?DeviceStatus=T
-curl http://172.19.0.13:8080/update/4?DeviceInformation=TEST_UPDATE_INFO
