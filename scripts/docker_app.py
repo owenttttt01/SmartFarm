0a858b3b091479e50490dcdcf9f6d716a83bd5e9
owenttttt01 <82928400+owenttttt01@users.noreply.github.com>
2021-08-03 11:26:17 +0800

    update docker_app.py

--- scripts/docker_app.py
+++ scripts/docker_app.py
@@ -7,14 +7,20 @@ import random
 import MySQLdb
 import os
 import re
-# from os import urandom
-# from Crypto.Cipher import AES
+from cryptography.fernet import Fernet
+import base64
 
 app = Flask(__name__)
 db_user = "sfuser"
 db_password = "sfp@ssw0rD$su$"
 db_address = "localhost"
 db_database = "SmartFarm"
+
+#Retrieve key for encryption/decryption from file
+file = open('key.txt')
+key = file.readline()
+#Initialise Fernet with key
+fernet = Fernet(key)
 
 @app.route('/init')
 def init():
@@ -77,6 +83,11 @@ def update(Id=None):
         check = check + 1
         print("Failed to update. Incorrect Device Information Format: "+device_informationStr, "\n")
     
+    #Encrypt encoded data before updating db
+    encrypt_time = fernet.encrypt(device_timeStr.encode())
+    encrypt_status = fernet.encrypt(device_statusStr.encode())
+    encrypt_info = fernet.encrypt(device_informationStr.encode())
+
     if check == 0:
         try:
             db = MySQLdb.connect(db_address, db_user, db_password, db_database)
@@ -89,9 +100,9 @@ def update(Id=None):
             #query="update sensorsUp set DeviceTime = '" + db.escape_string(device_timeStr).decode("UTF-8") + "', DeviceStatus = '" + db.escape_string(device_statusStr).decode("UTF-8") + "', DeviceInformation = '" + db.escape_string(device_informationStr).decode("UTF-8") + "' where Id = '" + db.escape_string(Id).decode("UTF-8") + "';"
             #Prepared Statement
             query="update sensorsUp set DeviceTime = %s, DeviceStatus = %s, DeviceInformation = %s where Id = %s;"
-            q1 = (device_timeStr, device_statusStr, device_informationStr, Id)
+            #q1 = (device_timeStr, device_statusStr, device_informationStr, Id)
+            q1 = (encrypt_time, encrypt_status, encrypt_info, Id)
             cursor.execute(query, q1)
-            print("UPDATE", q1)
             # print(query)
             # cursor.execute(query)
             db.commit()
@@ -99,7 +110,7 @@ def update(Id=None):
             db.rollback()
             return "FAIL\n"
         db.close()
-        return "UPDATED\n"
+        return "UPDATED SUCCESSFULLY\n"
     return "NOT UPDATED\n"
 
 @app.route("/add/", methods=["GET","PUT"] )
@@ -143,16 +154,11 @@ def add(device_name=None):
         check = check + 1
         print("Failed to add. Incorrect Device Information Format: "+device_informationStr, "\n")
 
-    # #secret_key = urandom(16).encode("UTF8")
-    # key = "abcdefghijklmnop"
-    # secret_key = str.encode(key)
-    # #iv = urandom(16).encode("UTF8")
-    # iv = str.encode(key)
-    # aes_en = AES.new(secret_key, AES.MODE_CBC, iv)
-    # encrypt_name = aes_en.encrypt(str.encode(device_name))
-    # encrypt_status = aes_en.encrypt(str.encode(device_statusStr))
-    # encrypt_time = aes_en.encrypt(str.encode(device_timeStr))
-    # encrypt_info = aes_en.encrypt(str.encode(device_informationStr))
+    #Encrypt encoded data before adding to db
+    encrypt_name = fernet.encrypt(device_name.encode())
+    encrypt_time = fernet.encrypt(device_timeStr.encode())
+    encrypt_status = fernet.encrypt(device_statusStr.encode())
+    encrypt_info = fernet.encrypt(device_informationStr.encode())
 
     if check == 0:
         try:
@@ -164,17 +170,16 @@ def add(device_name=None):
             #query="insert into sensors(Device,DeviceTime,DeviceStatus,DeviceInformation) values ('" + db.escape_string(device_name).decode("UTF-8") + "','" + db.escape_string(device_timeStr).decode("UTF-8") + "','" + db.escape_string(device_statusStr).decode("UTF-8") + "','" + db.escape_string(device_informationStr).decode("UTF-8") + "');"
             #Prepared Statement
             query="insert into sensors(Device,DeviceTime,DeviceStatus,DeviceInformation) values (%s,%s,%s,%s);"
-            q1 = (device_name, device_timeStr, device_statusStr, device_informationStr)
-            #q1 = (encrypt_name, encrypt_time, encrypt_status, encrypt_info)
+            #q1 = (device_name, device_timeStr, device_statusStr, device_informationStr)
+            q1 = (encrypt_name, encrypt_time, encrypt_status, encrypt_info)
             cursor.execute(query, q1)
-            print("INSERT", q1)
             db.commit()
         except Exception as e:
             db.rollback()
             print(str(e))
             return "FAIL\n"
         db.close()
-        return "ADDED\n"
+        return "ADDED SUCCESSFULLY\n"
     return "NOT ADDED\n"
     
 # @app.route("/")
@@ -191,9 +196,30 @@ def default():
         return "MYSQL not running"
     cursor = db.cursor()
     cursor.execute('''select * from sensors''')
-    comments = cursor.fetchall()
+    comments1 = cursor.fetchall()
+    comments2 = []
+
+    #Loop through each database entry
+    for row in comments1:
+        id, name, time, status, info = row[0], row[1], row[2], row[3], row[4]
+        
+        #Convert string to bytes
+        name2 = bytes(name, 'utf-8')
+        time2 = bytes(time, 'utf-8')
+        status2 = bytes(status, 'utf-8')
+        info2 = bytes(info, 'utf-8')
+
+        #Decrypt and decode data
+        decrypt_name = fernet.decrypt(name2).decode()
+        decrypt_time = fernet.decrypt(time2).decode()
+        decrypt_status = fernet.decrypt(status2).decode()
+        decrypt_info = fernet.decrypt(info2).decode()
+
+        #Store decrypted data in new array
+        comments2.append((id, decrypt_name, decrypt_time, decrypt_status, decrypt_info))
+
     db.close()
-    return render_template("db.html", comments=comments)
+    return render_template("db.html", comments2=comments2)
 
 @app.route("/")
 def dashboard():
@@ -203,9 +229,20 @@ def dashboard():
         return "MYSQL not running"
     cursor = db.cursor()
     cursor.execute('''select Device, DeviceStatus from sensorsUp''')
-    comments = cursor.fetchall()
+    comments1 = cursor.fetchall()
+    comments2 = []
+    for row in comments1:
+        name, status = row[0], row[1]
+
+        if status == "OFF":
+            comments2.append((name, status))
+        else:
+            status2 = bytes(status, 'utf-8')
+            decrypt_status = fernet.decrypt(status2).decode()
+            comments2.append((name, decrypt_status))
+
     db.close()
-    return render_template("dashboard.html", comments=comments)
+    return render_template("dashboard.html", comments2=comments2)
 
 if __name__ == "__main__":
     host = os.getenv("IP", "0.0.0.0")
